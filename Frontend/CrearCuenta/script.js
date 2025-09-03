3dc34af375203a7635262893f614694c91307cf5
Alex <mulleralex204@gmail.com>
2025-09-03 14:08:00 -0300

    Add files via upload

---
+++ Frontend/CrearCuenta/script.js
@@ -0,0 +1,6 @@
+let NombreDeUsuario = document.getElementById ("button-entrar")
+let Mail = document.getElementById ("mail")
+let contraseña = document.getElementById ("Contraseña")
+let FechaDeNacimiento = document.getElementById ("calendario")
+let Terminos = document.getElementById ("Terminos")
+
---
+++ Frontend/InicioSesion/script.js
@@ -0,0 +1,26 @@
+
+
+const emailValido = "test@mail.com";
+const contraseñaValida = "1234";
+
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    let email = document.getElementById("email").value.trim();
+    let contraseña = document.getElementById("contraseña").value.trim();
+    let mensaje = document.getElementById("mensaje");
+
+    
+    if (email === "" || contraseña === "") {
+        mensaje.textContent = "⚠️ Por favor, completa todos los campos.";
+        mensaje.style.color = "red";
+        return;
+    }
+
+
+    if (email === emailValido && contraseña === contraseñaValida) {
+        window.location.href = "../menu principal/indexMenuPrincipal.html";
+    } else {
+        mensaje.textContent = "❌ Usuario o contraseña incorrectos.";
+        mensaje.style.color = "red";
+    }
+});
