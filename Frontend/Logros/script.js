c6b38f80c543976b5e69627f33748d3df146eed3
Lucas Schniper <you@example.com>
2025-10-22 15:52:14 -0300

    avance algo los logros

--- Frontend/Logros/script.js
+++ Frontend/Logros/script.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menuBtn");
 const sidebar = document.getElementById("sidebar");
 const overlay = document.getElementById("overlay");
 
+
 menuBtn.addEventListener("click", () => {
   sidebar.classList.toggle("open");
   overlay.classList.toggle("show");
@@ -11,3 +12,4 @@ overlay.addEventListener("click", () => {
   sidebar.classList.remove("open");
   overlay.classList.remove("show");
 });
+
