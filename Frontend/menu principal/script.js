662fd245f95656efd5b41e851d92c26103ac6faa
LucasSchniper <lucasschniper@gmail.com>
2025-10-23 21:09:53 -0300

    se boton crear y css del menu principal

--- Frontend/menu principal/script.js
+++ Frontend/menu principal/script.js
@@ -11,3 +11,14 @@ overlay.addEventListener("click", () => {
   sidebar.classList.remove("open");
   overlay.classList.remove("show");
 });
+
+let botonCrear = document.getElementById('botonCrear');
+
+
+let rutaCrearObjetivo = '..//Creacion de Objetivos/IndexCreacionDeObjetivos.html';
+
+if (botonCrear) {
+  botonCrear.addEventListener('click', function () {
+    window.location.href = rutaCrearObjetivo;
+  });
+}
