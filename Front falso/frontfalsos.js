ab883785c20f800c590c7c1400060d4e1578b0ad
Muller912 <mulleralex204@gmail.com>
2025-10-24 15:10:33 -0300

    Funcionan los nuevos ID y el frontfalso de crear

--- Front falso/frontfalsos.js
+++ Front falso/frontfalsos.js
@@ -1,10 +1,23 @@
-import fs from "fs";
-connect2server(3000);
+
+let usuario = "Luis";
+let mail = "luis@gmail.com";
+let contraseña = "JAJAJA23  ";
+let fecha = "20/06/2024";
+connect2Server(3000);
 //Copiar y Pegar
 postEvent("", {}, (data) => {
 
 })
-postEvent("crear", {usuario, mail, contraseña, fecha}, (data) => {})
+document.getElementById("crear").addEventListener("click", () => {
+  postEvent("crear", { usuario, mail, contraseña, fecha }, (data) => {
+    if (data.ok) {
+      console.log("Usuario creado");
+    } else {
+      console.log("Error al crear usuario");
+    }
+  });
+});
+
 
 
 
