89f99274be210fb70fd98f261a13e4100e1090f3
Lucas Schniper <you@example.com>
2025-10-29 10:40:54 -0300

    YA ME ANDA SOQUETIC DE CREAR CUENTA Y INICO SESION

--- Frontend/InicioSesion/script.js
+++ Frontend/InicioSesion/script.js
@@ -1,39 +1,31 @@
 
 
-   
+   connect2Server(3000);
 
 document.getElementById("loginForm").addEventListener("submit", function(e) {
     e.preventDefault(); 
-    let email = document.getElementById("email").value.trim();
+    let mail = document.getElementById("email").value.trim();
     let contraseña = document.getElementById("contraseña").value.trim();
     let mensaje = document.getElementById("mensaje");
 
     
-    if (email === "" || contraseña === "") {
+    if (mail === "" || contraseña === "") {
         mensaje.textContent = " Por favor, completa todos los campos.";
         mensaje.style.color = "red";
         return;
     }
 
 
-    if (email === emailValido && contraseña === contraseñaValida) {
+        postEvent("login", {mail, contraseña}, (data) => {
+          if (data.ok === true) {
+        alert("Inicio de sesión exitoso");
         window.location.href = "../menu principal/indexMenuPrincipal.html";
-    } else {
-        mensaje.textContent = " Usuario o contraseña incorrectos.";
-        mensaje.style.color = "red";
-    }
-
-    let valido =false;
-    if(valido){
-        postEvent("crear", {mail, contraseña}, (objok) => {
-          if (objok.ok === true) {
-            alert("El usuario ha sido creado exitosamente");
           } else {
-            alert("Hubo un error al crear el usuario");
-       
+            mensaje.textContent = " Usuario o contraseña incorrectos.";
+            mensaje.style.color = "red";
         }
     });
-}
+
 });
 
 
