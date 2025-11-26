b911e5e30d8e7b521f1b6e63e26c8bfbdfedadcf
Your Name <mulleralex204@gmail.com>
2025-11-26 12:07:44 -0300

    JAROSET

--- Frontend/CrearCuenta/script.js
+++ Frontend/CrearCuenta/script.js
@@ -71,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
     postEvent("crear", {usuario, mail, contraseña, fecha}, (objok) => {
       if (objok.ok === true) {
         alert("El usuario ha sido creado exitosamente");
-        window.location.href = "../crearcuentaoiniciosesion/indexcrear.html";
+        window.location.href = "../crearcuentaoiniciarsesion/IndexCrearCuentaInicioSesion.html";
       } else {
         alert("Hubo un error al crear el usuario");
       }
